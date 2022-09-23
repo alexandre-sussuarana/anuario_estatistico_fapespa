5b508dfffa3df47b357b271b1da344eb2fc26f85
alexandre-sussuarana <alexandrehsussuarana@gmail.com>
2022-09-22 21:43:30 -0300

    adição da última versão do script 0_downloads. Este script será removido após a atualização do anuário 2021.

--- 0_download_tabelas.R
+++ 0_download_tabelas.R
@@ -1,8 +1,164 @@
-# ANUARIO 2021 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
 # PACOTES ---------------------------------------------------------------------
 
 library(dplyr)
+
+# ANUARIO 2015 ----------------------------------------------------------------
+
+# an15 <- NULL                         # OBJETO LIST COM A TOTALIDADE DAS TABELAS
+
+baixar = function(                # FUNCAO DE DOWNLOAD E TRATAMENTO DAS TABELAS
+    extensao = NA,
+    setor = NA,
+    subsetor = NA,
+    link = NA
+) {
+  
+  x = NULL
+  
+  temp <- tempfile(pattern = 'tabela_', fileext = '.xlsx')  # OBJETO TEMPORARIO
+  
+  for (i in extensao) {                       # DOWNLOAD E TRATAMENTO DA TABELA
+    
+    download.file(                                        # DOWNLOAD  DA TABELA
+      url = paste0(gsub('Tabela..*', 'Tabela', link),
+                   i,
+                   gsub('.*Tabela.', '', link)),
+      destfile = temp,
+      method = 'libcurl',
+      mode = 'wb'
+    )
+    
+    d = readxl::read_excel(
+      path = temp,
+      col_names = F,
+      na = c('-', '\\*') #,
+      #skip = 2,
+      #n_max = 145
+    )
+    
+    nometab = d$...1[1]
+    
+    inicio.col = grep('^Estado', d$...1)
+    
+    if (length(inicio.col) == 0) {
+      inicio.col = which(is.na(d$...2) == F) |> dplyr::first()
+      d$...1[inicio.col] = 'Estado/Município'
+    }
+    
+    fim = which(is.na(d$...1)) |> dplyr::last()-1
+    
+    if (ncol(d) > 8) {
+      
+      nomecol = d[inicio.col:(inicio.col+1), ] |> t() |> data.frame()
+      
+      guias = which(is.na(nomecol$X1) == F)
+      
+      for (j in which(is.na(nomecol$X1))) {
+        nomecol$X1[j] = nomecol$X1[guias[which(guias < j)] |> dplyr::last()]
+        rm(j)
+      }
+      
+      nomecol = c(nomecol$X1[1], paste(nomecol$X1[-1], '-', nomecol$X2[-1]))
+      
+      names(d) = nomecol
+      
+      rm(nomecol, guias)
+      
+    } else {
+      
+      names(d) = gsub('\\*',
+                      '',
+                      d[inicio.col, ])
+    }
+    
+    d = d[(inicio.col+1):fim, ]
+    
+    if (is.na(subsetor)) {
+      x[[paste0(setor, ' - ', nometab)]] = d
+    } else {
+      x[[paste0(setor, ' - ', subsetor)]][[nometab]] = d
+    }
+    
+    
+    rm(i, d, nometab, inicio.col, fim)
+    
+  }                   # DOWNLOAD E TRATAMENTO DA TABELA
+  
+  x
+  
+}
+
+## demografia -----------------------------------------------------------------
+
+ # O Loop a seguir baixa as tabelas e as acomoda em um objeto list.
+
+demo = baixar(
+  extensao = 1:9,
+  setor = 'Demografia',
+  link = 'https://www.fapespa.pa.gov.br/sistemas/anuario2015/relatorios/demografia/xlsx/Tabela1_De.xlsx')
+
+for (i in 1:9) {                                        # VISUALIZAR AS TABELAS
+  View(demo[[i]], title = paste0('tab_', i))
+  rm(i)
+}                                    # VISUALIZAR AS TABELAS
+
+
+d |> tidyr::pivot_longer(cols = c(-1),
+                      names_to = 'periodo',
+                      values_to = 'valor') |> View()
+
+saveRDS(object = an15, file = 'Anuarios_RDS/Anuario_2015.RDS')
+
+## economia -------------------------------------------------------------------
+
+  # OBS: Aparentemente, as tabelas se repetem a partir da 6ª. Logo, o list con-
+  # tém apenas 5 tabelas, já que 3 estão repetidas.
+
+econ = baixar(
+  extensao = 1:8,
+  setor = 'Economia',
+  subsetor = 'PIB',
+  link = 'https://www.fapespa.pa.gov.br/sistemas/anuario2015/relatorios/economia/pib/xlsx/Tabela1_Pib.xlsx'
+  )
+
+for (i in 1:length(econ[[1]])) {
+  View(econ[[1]][[i]], title = paste0('tab_', i))
+  rm(i)
+}
+
+
+## salvar arquivos ------------------------------------------------------------
+
+an15 = list(
+  'demografia' = demo,
+  'economia' = econ
+  )
+
+saveRDS(object = an15, file = 'Anuarios_RDS/Anuario_2015.RDS')
+
+
+# ANUARIO 2018 ----------------------------------------------------------------
+
+## demografia -----------------------------------------------------------------
+
+links = c(
+  'https://www.fapespa.pa.gov.br/sistemas/anuario2018//xls/demografia/tab_1.1_estimativas_populacionais_para_e_municipios_2013_a_2017.xlsx',
+  'https://www.fapespa.pa.gov.br/sistemas/anuario2018/xls/demografia/tab_1.2_populacao_por_faixa_etaria_para_e_municipios_2011_a_2015.xlsx',
+  'https://www.fapespa.pa.gov.br/sistemas/anuario2018/xls/demografia/tab_1.3_populacao_por_sexo_para_e_municipios_2011_a_2015.xlsx',
+  'https://www.fapespa.pa.gov.br/sistemas/anuario2018//xls/demografia/tab_1.4_razao_de_sexos_para_e_municipios_2011_a_2015.xlsx',
+  'https://www.fapespa.pa.gov.br/sistemas/anuario2018//xls/demografia/tab_1.5_proporcao_de_idosos_para_e_municipios_2011_a_2015.xlsx',
+  'https://www.fapespa.pa.gov.br/sistemas/anuario2018//xls/demografia/tab_1.6_indice_de_envelhecimento_para_e_municipios_2011_a_2015.xlsx',
+  'https://www.fapespa.pa.gov.br/sistemas/anuario2018/xls/demografia/tab_1.7_razao_de_dependencia_para_e_municipios_2011_a_2015.xlsx',
+  'https://www.fapespa.pa.gov.br/sistemas/anuario2018/tabelas/demografia/tab_1.8_taxa_de_fecundidade_total_para_e_municipios_2011_a_2015.htm',
+  'https://www.fapespa.pa.gov.br/sistemas/anuario2018/xls/demografia/tab_1.9_taxa_especifica_de_fecundidade_por_faixa_etaria_para_e_municipios_2011_a_2015.xlsx'
+)
+
+
+
+# ANUARIO 2021 ----------------------------------------------------------------
+
+# Percebi que cada anuario tem sua particularidade. Preciso vence-los um a um
 
 # DOWNLOAD E EXTRACAO ---------------------------------------------------------
 
@@ -191,3 +347,10 @@ for (j in 1:nrow(tab[[1]])) {
 
 saveRDS(object = dm,                                  # SALVAR O ARQUIVO EM RDS
         file = '1_demografia.RDS')
+
+writexl::write_xlsx(     # SALVAR O ARQUIVO EM XLSX. CADA PLANILHA É UMA TABELA
+  x = dm$demografia,
+  path = '1_demografia.xlsx',
+  col_names = T,
+  format_headers = T
+  )
